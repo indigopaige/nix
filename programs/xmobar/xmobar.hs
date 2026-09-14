4ee949cff9a0924ea85efd9f013b50065efd6b8d
indigo <indigojmartinez@icloud.com>
2026-09-14 17:09:07 -0500

    rearrange xmobar

--- programs/xmobar/xmobar.hs
+++ programs/xmobar/xmobar.hs
@@ -1,26 +1,40 @@
 Config
   { overrideRedirect = False
   , font     = "xft:Iosevka:size=20:antialias=true"
-  , bgColor  = "#1e1e2e"
-  , fgColor  = "#cdd6f4"
+  , bgColor  = "#303446"
+  , fgColor  = "#c6d0f5"
   , position = TopSize C 100 32
-  , commands = [ Run Cpu
-                 [ "-L", "3"
-                 , "-H", "50"
-                 , "--high"  , "#f38ba8"
-                 , "--normal", "#a6e3a1"
-                 ] 10
-               , Run Alsa "default" "Master"
-                 [ "--template", "<volumestatus>"
-                 , "--suffix"  , "True"
-                 , "--"
-                 , "--on", ""
-                 ]
-               , Run Memory ["--template", "Mem: <fc=#cba6f7><usedratio></fc>%"] 10 
-               , Run Date "%a %Y-%m-%d <fc=#89b4fa>%H:%M</fc>" "date" 10 
-               , Run XMonadLog
-               ]
+  , commands =
+      [ Run Cpu
+          [ "-L", "3"
+          , "-H", "50"
+          , "--high"  , "#e78284"
+          , "--normal", "#a6d189"
+          ] 10
+
+      , Run Alsa "default" "Master"
+          [ "--template", "<volumestatus>"
+          , "--suffix"  , "True"
+          , "--"
+          , "--on", ""
+          ]
+
+      , Run Memory
+          [ "--template"
+          , "Mem: <fc=#ca9ee6><usedratio></fc>%"
+          ] 10
+
+      , Run Date
+          "%a %Y-%m-%d <fc=#babbf1>%H:%M</fc>"
+          "date"
+          10
+
+      , Run XMonadLog
+      ]
+
   , sepChar  = "%"
   , alignSep = "}{"
-  , template = "%XMonadLog% }{ %alsa:default:Master% | %cpu% | %memory% | %date% "
+
+  , template =
+      "%alsa:default:Master% }{ %XMonadLog% | %date% }{ %cpu% | %memory%"
   }
