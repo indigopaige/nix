95baa5469f41e14b61b073edae14f2759427c98f
indigo <indigojmartinez@icloud.com>
2026-09-14 19:49:30 -0500

    manageHook

--- wm/xmonad/Main.hs
+++ wm/xmonad/Main.hs
@@ -8,10 +8,44 @@ import XMonad.Util.EZConfig
 import XMonad.Util.Loggers
 import XMonad
 
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " ★ "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "<") (white    ">") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "<") (lowWhite ">") . blue    . ppWindow
+
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#babbf1" ""
+    blue     = xmobarColor "#8caaee" ""
+    white    = xmobarColor "#c6d0f5" ""
+    yellow   = xmobarColor "#e5c890" ""
+    red      = xmobarColor "#e78284" ""
+    lowWhite = xmobarColor "#a5adce" ""
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+  [ className =? "emacs"   --> doShift "1"
+  , className =? "vesktop" --> doShift "2"
+  , className =? "Mail"    --> doShift "3"
+  ]
+
 myConfig = def
   { focusedBorderColor = "#babbf1"
   , normalBorderColor  = "#303446"
   , modMask            = mod4Mask
+  , manageHook         = myManageHook
   , startupHook        = do
       spawn "feh --bg-fill --randomize ~/Pictures/Wallpapers/*"
       pure ()
@@ -31,34 +65,6 @@ myConfig = def
                  , "2"
                  , "3"
                  ]
-
-
-myXmobarPP :: PP
-myXmobarPP = def
-    { ppSep             = magenta " ★ "
-    , ppTitleSanitize   = xmobarStrip
-    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
-    , ppHidden          = white . wrap " " ""
-    , ppHiddenNoWindows = lowWhite . wrap " " ""
-    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
-    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
-    , ppExtras          = [logTitles formatFocused formatUnfocused]
-    }
-  where
-    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
-    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
-
-    ppWindow :: String -> String
-    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
-
-    blue, lowWhite, magenta, red, white, yellow :: String -> String
-    magenta  = xmobarColor "#babbf1" ""
-    blue     = xmobarColor "#8caaee" ""
-    white    = xmobarColor "#c6d0f5" ""
-    yellow   = xmobarColor "#e5c890" ""
-    red      = xmobarColor "#e78284" ""
-    lowWhite = xmobarColor "#a5adce" ""
-
 
 main :: IO ()
 main = xmonad
